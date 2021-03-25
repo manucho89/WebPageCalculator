896a99eef07de9a7336df7e20910f477f4266a1f
Your Name <you@example.com>
2021-03-25 10:49:26 +0100

    primer commit

---
+++ complete/src/main/java/com/example/servingwebcontent/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+class HelloController {
+
+    /*@RequestMapping("/")
+    public String index() {
+        return "GfQn3hVmLuVa95ogAyiK";
+    }*/
+
+    @RequestMapping("/suma")
+    public int index(int x, int y) {
+        return x + y;
+    }
+
+}
